f3588209c3066ce892bca2c1ce825445a0ae0d13
lamden000 <lamden150@gmail.com>
2024-10-28 15:26:01 +0700

    fix bugs

--- 22520737/ViewModels/HomeProductDetailViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Thuchanh2.Models;
-namespace Thuchanh2.ViewModels
-{
-    public class HomeProductDetailViewModel
-    {
-        public TDanhMucSp danhMucSp {  get; set; }
-        public List<TAnhSp> anhSps {  set; get; }
-    }
-}
